f6e899655893495b3572239ee3dc3787c7e8e10f
hitomipupil <hitomi0727pupil@gmail.com>
2026-07-03 15:28:39 +0200

    added layout

---
+++ src/layout/MainApp.tsx
@@ -0,0 +1,11 @@
+import { AppHeader } from '../components/AppHeader'
+import { ScheduleView } from '../features/schedule/ScheduleView'
+
+export const MainApp = () => {
+  return (
+    <>
+      <AppHeader />
+      <ScheduleView />
+    </>
+  )
+}
