aa91c0a0b52ecb92145aa7e20da86814599e2a9c
hitomipupil <hitomi0727pupil@gmail.com>
2026-07-03 15:37:39 +0200

    updated UI

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -4,9 +4,15 @@ import { useCurrentUser } from '../contexts/useCurrentUser'
 export const AppHeader = () => {
   const { currentUser } = useCurrentUser()
   return (
-    <Box sx={{ flexDirection: 'row' }}>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+    >
       <Typography variant="h6" component="div">
-        Shift Management App
+        Shift Management
       </Typography>
       <Typography>{`${currentUser.name} - ${currentUser.role}`}</Typography>
     </Box>
