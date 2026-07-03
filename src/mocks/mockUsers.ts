5c32fe0b09e8c1e6bfa1f8074ac40eb654be3c98
hitomipupil <hitomi0727pupil@gmail.com>
2026-07-03 14:29:49 +0200

    added mock users

---
+++ src/mocks/mockUsers.ts
@@ -0,0 +1,19 @@
+import type { User } from '../types/user'
+
+export const mockUsers: User[] = [
+  {
+    id: 'manager-1',
+    name: 'Hitomi',
+    role: 'manager',
+  },
+  {
+    id: 'employee-1',
+    name: 'Alice',
+    role: 'employee',
+  },
+  {
+    id: 'employee-2',
+    name: 'Bob',
+    role: 'employee',
+  },
+]
