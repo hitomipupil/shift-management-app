3994fcf486641dc9a4ad58250950ef330d16ba24
hitomipupil <hitomi0727pupil@gmail.com>
2026-07-04 12:50:06 +0200

    added WeeklyScheduleSectino

---
+++ src/features/schedule/WeeklyScheduleSection.tsx
@@ -0,0 +1,41 @@
+import type { Shift } from 'src/types/shift'
+import { ShiftCard } from './ShiftCard'
+import type { User } from 'src/types/user'
+import { Typography } from '@mui/material'
+
+type WeeklyScheduleSectionProps = {
+  shifts: Shift[]
+  users: User[]
+}
+
+export const WeeklyScheduleSection = ({
+  shifts,
+  users,
+}: WeeklyScheduleSectionProps) => {
+  return (
+    <>
+      <Typography>Weekly Schedule</Typography>
+      {shifts.length === 0 ? (
+        <Typography color="text.secondary">No shifts this week.</Typography>
+      ) : (
+        shifts.map((shift) => {
+          const assignedUser = users.find(
+            (user) => user.id === shift.assignedUserId,
+          )
+
+          if (!assignedUser) {
+            throw new Error(`Assigned user not found: ${shift.assignedUserId}`)
+          }
+
+          return (
+            <ShiftCard
+              key={shift.id}
+              shift={shift}
+              assignedUserName={assignedUser.name}
+            />
+          )
+        })
+      )}
+    </>
+  )
+}
