a728265061f2a394124fad842eff8de942e5f9f1
hitomipupil <hitomi0727pupil@gmail.com>
2026-07-05 20:01:33 +0200

    added return value type

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,13 +1,14 @@
 import { mockUsers } from 'src/mocks/mockUsers'
+import type { User } from 'src/types/user'
 
-export const getUsers = async () => {
+export const getUsers = async (): Promise<User[]> => {
   return mockUsers
 }
 
-export const getUserById = async (userId: string) => {
+export const getUserById = async (userId: string): Promise<User | null> => {
   return mockUsers.find((user) => user.id === userId) ?? null
 }
 
-export const userExists = async (userId: string) => {
+export const userExists = async (userId: string): Promise<boolean> => {
   return mockUsers.some((user) => user.id === userId)
 }
