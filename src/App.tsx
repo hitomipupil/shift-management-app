bede77efeac2a04a954442a1cb94ce0f48a1b73f
hitomipupil <hitomi0727pupil@gmail.com>
2026-07-06 09:27:28 +0200

    Improve auth loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import { Typography } from '@mui/material'
 import { UserSelector } from 'src/components/UserSelector'
 import { CurrentUserProvider } from 'src/contexts/CurrentUserProvider'
 import { useCurrentUser } from 'src/contexts/useCurrentUser'
 import { MainApp } from 'src/features/layout/MainApp'
 
 const AppContent = () => {
-  const { currentUser } = useCurrentUser()
+  const { currentUser, isLoadingCurrentUser, currentUserError } =
+    useCurrentUser()
+
+  if (isLoadingCurrentUser) {
+    return <Typography>Loading...</Typography>
+  }
+
+  if (currentUserError) {
+    return <Typography color="error">{currentUserError}</Typography>
+  }
+
   if (!currentUser) {
     return <UserSelector />
   }
+
   return <MainApp />
 }
 
